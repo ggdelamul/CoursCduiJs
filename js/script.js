9b5fcc22be8e7dce4dc73c753e53afa609cebdb1
jeremy <projeremylegendre@gmail.com>
2024-03-05 20:28:17 +0100

    notionVariable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("coucou ✌️");
+//////////////////NOTION DE VARIABLE///////////////////////////////
+///récupérer le titre 
+const titre = document.querySelector('h1');
+console.log(titre);
+//modifier la couleur du titre 
+titre.style.color = 'red';
+let nom = "Jhon";
+let prenom = "Doe";
+let nomComplet = nom + " " + prenom;
+titre.textContent = nomComplet;
+let nombre = 42; 
+let monBooleen = true ; 
+//pratique 
+//créer un paragraphe dans le doc html 
+//créer 2 variable a et b  nombre les multiplier entre eux 
+//Composer une chaine de caractère et et l'inclure dans la balise paragraphe sous cette forme "le resultat de a et de = resultat"; 
