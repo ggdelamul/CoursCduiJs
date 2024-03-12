6d8dce2970bcbcbddbc5dbfa146bafb9d525ab54
jeremy <projeremylegendre@gmail.com>
2024-03-12 15:54:19 +0100

    modif-1

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ console.log("coucou ✌️");
 ///récupérer le titre 
 const titre = document.querySelector('h1');
 console.log(titre);
-//modifier la couleur du titre 
-titre.style.color = 'red';
-let nom = "Jhon";
+
+
+let nom = "Jhon";//chaine de caractère 
 let prenom = "Doe";
+let nombre = 42; //nombre , peux être un nombre décimal 
+let monBooleen = true; // booléen
+//modifier le contenu du titre  
 let nomComplet = nom + " " + prenom;
 titre.textContent = nomComplet;
-let nombre = 42; 
-let monBooleen = true ; 
-//pratique 
-//créer un paragraphe dans le doc html 
-//créer 2 variable a et b  nombre les multiplier entre eux 
-//Composer une chaine de caractère et et l'inclure dans la balise paragraphe sous cette forme "le resultat de a et de = resultat"; 
+//modifier la couleur du titre 
+titre.style.color = 'red';
+//pratique
+//créer un paragraphe dans le doc html
+//créer 2 variable a et b  nombre les multiplier entre eux
+//Composer une chaine de caractère et et l'inclure dans la balise paragraphe sous cette forme "le resultat de a et de b = resultat"; 
+
